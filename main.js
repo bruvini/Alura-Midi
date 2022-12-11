a84559e004f5419638d350fe120c881fada24830
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-11 16:25:07 -0300

    parâmetros e funções anônimas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudio){
+    document.querySelector(idAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,7 +7,9 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let cont = 0;
 
 while (cont < listaDeTeclas.length) {
-    listaDeTeclas[cont].onclick = tocaSomPom;
+    listaDeTeclas[cont].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    };
     cont = cont + 1;
     console.log(cont);
 }
